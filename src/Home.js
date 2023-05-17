f4f6e40248fa458522f0004a9f05daa343fc828e
Dawan Sawyer <dawansawyer@gmail.com>
2023-05-16 22:56:01 -0400

    created file for routes and started home page

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Attack on Titan Fan Page</h1>
+      <p>Explore the world of Attack on Titan with us!</p>
+    </div>
+  );
+}
+
+export default Home;
