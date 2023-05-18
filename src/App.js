9183aeb557c8aeab6f63c51df821be317002e525
Dawan Sawyer <dawansawyer@gmail.com>
2023-05-18 07:58:27 -0400

    imported Routes instead of Switch for routes to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavigationBar from './NavigationBar';
 import Home from './Home';
 import CharacterEncyclopedia from './CharacterEncyclopedia'
@@ -10,12 +10,12 @@ const App = () => {
   return (
     <Router>
       <NavigationBar />
-      <Switch>
+      <Routes>
         <Route exact path="/" Component={Home} />
         <Route path="/characters" Component={CharacterEncyclopedia} />
         <Route path="/episodes" Component={EpisodeGuide} />
         <Route path="/quiz" Component={QuizGame} />
-      </Switch>
+      </Routes>
     </Router>
   );
 }
