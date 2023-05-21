1bece9cc2e7d3db313d63aeb1e0968a9e19e841c
Dawan Sawyer <dawansawyer@gmail.com>
2023-05-21 17:47:06 -0400

    I now have properly passed props from Home.js to FeaturedCharacters.js, and i started building out home page boiler plate

--- src/FeaturedCharacters.js
+++ src/FeaturedCharacters.js
@@ -1,18 +1,11 @@
 import React from "react";
 
-const FeaturedCharacters = () => {
-  // Featured Characters Data
-  const characters = [
-    { id: 1, name: "Eren Yeager", image: "eren.jpg" },
-    { id: 2, name: "Mikasa Ackerman", image: "mikasa.jpg" },
-    // Add the rest of the characters here
-  ];
-
+const FeaturedCharacters = ({characters}) => {
   return (
     <div>
       <h3>Featured Characters</h3>
       <div className="character-list">
-        {characters.map(character => (
+        {characters.map((character) => (
           <div key={character.id} className="character-item">
             <img src={character.image} alt={character.name} />
             <p>{character.name}</p>
--- src/Home.js
+++ src/Home.js
@@ -2,14 +2,31 @@ import React from "react";
 import FeaturedCharacters from "./FeaturedCharacters";
 
 const Home = () => {
-  return (
-    <div>
-      <h2>Welcome to Attack on Titan Fan Page</h2>
-      <FeaturedCharacters />
-      { /* Recent Episodes */ }
 
+  const characters = [
+      // Featured Characters Data
+    {id: 1, name: "Eren Yeager", image:"eren.jpg" },
+    /*{id: 2, name: "Mikasa Ackerman", image: "mikasa.jpg" },*/
+    
+    // Add the rest of the characters
+  ];
+
+  console.log(characters)
+
+  return (
+    <div className="home-container">
+      <FeaturedCharacters characters={characters} className="featured-characters" />
+      <div className="section character-section">
+        {/* Character Section Content Goes Here */}
+      </div>
+      <div className="section episode-section">
+        {/* Episode Section Content Goes Here */}
+      </div>
+      <div className="section quiz-section">
+        {/* Quiz Section Content Goes Here */}
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
