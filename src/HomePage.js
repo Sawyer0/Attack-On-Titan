5aaf268d7925acf98ff79c89552024db5e9f598e
Dawan Sawyer <dawansawyer@gmail.com>
2023-06-03 21:32:09 -0400

    created cards for the characters that are being displayed, shwoing only the data I need

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import CharacterCard from './CharacterCard';
 import axios from 'axios';
 
 const HomePage = () => {
@@ -23,67 +24,11 @@ const HomePage = () => {
     <div>
       <h1>Attack on Titan Characters</h1>
       {characters.map((character) => (
-        <div key={character.id}>
-          <h2>{character.name}</h2>
-          <p>Alias : {character.alias}</p>
-          <p>Birthplace: {character.birthplace}</p>
-          <p>Gender: {character.gender}</p>
-          <h3>Groups:</h3>
-          <ul>
-            {character.groups.map((group) => (
-              <li key={group.name}>
-                {group.name}
-                {group.sub_groups && (
-                  <ul>
-                    {group.sub_groups.map((subGroup) => (
-                      <li key={subGroup}>{subGroup}</li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
-          <h3>Height:</h3>
-          <li> {character.height}</li>
-          <h3>Picture:</h3>
-          <li>Picture is here just not viisble yet {/*{character.img}*/}</li>
-          <h3>Occupation</h3>
-          <li>{character.occupation}</li>
-          <h3>Relatives:</h3>
-          <ul>
-            {character.relatives.map((relative) => (
-              <li key={relative.family}>
-                {relative.family}
-                {relative.members && (
-                  <ul>
-                    {relative.members.map((member) => (
-                      <li key={member}>{member}</li>
-                    ))}
-                  </ul>
-                )}
-              </li>
-            ))}
-          </ul>
-          <p> Residence:  {character.residence}</p>
-          <h3>Roles: </h3>
-          <ul>
-            {character.roles.map((role) => (
-              <li key={role}>{role}</li>
-            ))}
-          </ul>
-          <h3>Species: </h3>
-          <ul>
-            {character.species.map((species) => (
-              <li key={species}>{species}</li>
-            ))}
-          </ul>
-          <h3>Status: </h3>
-          <li> {character.status}</li>
-          {/* Render other character information */}
-        </div>
+        <CharacterCard key={character.id} character={character} />
       ))}
     </div>
   );
 };
+
 
 export default HomePage;
